2bce36faf3ba73fcc48d7a5730c0548d9855fc5b
risification <sultangaziev01@bk.ru>
2020-12-21 21:02:29 +0600

    class_new_tema

--- tema_new.py
+++ tema_new.py
@@ -1,12 +1,16 @@
 class Car:
-    def __init__(self, name, model, price):
+
+    def __init__(self, name, model, price, speed):
         self.name = name
         self.model = model
         self.price = price
+        self.state = 100
         self.fuel = 100
         self.run = 0
         self.color = 'orange'
         self.no2 = False
+        self.broken = False
+        self.speed = speed
 
     def move(self, distance):
         if distance > 0:
@@ -15,9 +19,30 @@ class Car:
             self.fuel -= result
             self.run += distance
 
+    def broke(self, item):
+        if item == 'stolb':
+            self.state = self.state - 20
+            self.price -= 10000
+        elif item == 'car':
+            self.state = self.state - 40
+            self.price -= 20000
+        elif item == 'human':
+            self.state = self.state - 10
 
-car = Car('tesla', 'XS', 90000)
-print(car.run)
-car.move(240)
-print(car.run)
-print(car.fuel)
+    def check_broken(self):
+        if self.state < 50:
+            self.broken = True
+
+    def build(self):
+        self.no2 = True
+        self.speed += 30
+        self.price += 15000
+
+
+car = Car('tesla', 'XS', 90000, 390)
+car.broke('stolb')
+car.broke('stolb')
+car.broke('stolb')
+car.check_broken()
+car.build()
+print(car.broken, car.state, car.price, car.speed)
